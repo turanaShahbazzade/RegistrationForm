ccbd93704f03c07617bdbcd0565fb886e908a1a4
NahidaM <turaneshahbazzade@gmail.com>
2020-11-03 14:14:39 +0400

    I created registartion form

---
+++ Registration.Form/js/main.js
@@ -0,0 +1,4 @@
+let mailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let passwordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+let submit = document.querySelector("input[name='submit']");
+let termImportant = document.querySelector("")
